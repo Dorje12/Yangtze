b3f2d1321955bc5038236d8b4d0186462fef8d2c
Anish-Dangi <anishdangi999@gmail.com>
2022-06-18 13:10:46 +0545

    Create about.php

---
+++ about.php
@@ -0,0 +1,65 @@
+<div class="title">
+    <div class="pic1">
+    <img src="<?= base_url('public/images/Students.jpg'); ?>" alt="College Students image" class="university"/>
+    </div>
+    <div class="heading">
+    <p class="about"><b> ABOUT <br> US </b></p>
+    <p class="des">Woodlands University College provides modern and pratical way of eductation in an environment which is packed with immense energy and innovation which ultimately results in outstanding growth of our students. We are committed to excellence and consistency in every field. We provide all our staffs and students a helpful and friendly environment to guide them in the right path. Choosing us can be one of the best decisions you can ever make.
+    </p>        
+    </div>
+  </div>
+
+  <div class="vms">
+
+    <div class="vision">
+      <div class="vpic">
+      <img src="<?= base_url('public/images/vision.png'); ?>" alt="Vision" class="eyepic"/>
+
+      </div>
+      <div class="vtext">
+        <p class="vhead"><b> Our Vision </b></p>
+        <p class="vdes">To be the most reputed, recognized and reliable college in the U.K for the courses we provide.</p>
+      </div>
+    </div>
+
+    <div class="mission">
+      <div class="mtext">
+        <p class="mhead"><b> Our Mission </b></p>
+        <p class="mdes">To develop excellent graduates who can kickstart their career either as an employee in a well reputed company or as an entrepreneur. </p>
+      </div>
+      <div class="mpic">
+      <img src="<?= base_url('public/images/bowarrow1.png'); ?>" alt="Mission" class="targetpic"/>
+      </div>
+    </div>
+
+    <div class="strategy">
+      <div class="spic">
+       <img src="<?= base_url('public/images/strategy.png'); ?>" alt="Strategy" class="bulbpic"/>
+      </div>
+      <div class="stext">
+        <p class="shead"><b> Our Strategy </b></p>
+        <p class="sdes">To offer students the finest education by using most effective and efficient teaching method and providing all the resources necessary. </p>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="teaching">
+    <div class="tmtext">
+      <p class="tmhead"><b> TEACHING <br> APPROACH </b></p>
+      <p class="tmdes">We use special teching approach which is equally divided into lecture and pratical. This teaching approach oblige students to put knowledge into practice which they have gathered in lecture class which assists students to understand the topic more easily and cleraly. By following this method students also tends to remember the topic for a much longer period of time. </p>
+    </div>
+    <div class="tmpic">
+    <img src="<?= base_url('public/images/classroom.jpg'); ?>" alt="Classroom image" class="classroom"/>
+    </div>
+  </div>
+
+  <div class="student">
+    <div class="stupic">
+      <img src="<?= base_url('public/images/student.jpg'); ?>" alt="Student image" class="studentpic"/>
+    </div>
+    <div class="stutext">
+      <p class="stuhead"><b> STUDENT <br> LIFE </b></p>
+      <p class="studes">In Woodlands University College we believe that studying a course should always be more than just getting a degree. Here we offer a student life which you can personalize to your preferences. At university it's simple to engage yourself in exciting oppurtunities and experiences. Here you'll make lifelong friends, explore new possibilities and be more independent than you ever have before.</p>
+    </div>
+  </div>
